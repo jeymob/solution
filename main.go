d2b84a83cb4833e8b28975c1db72faa63ee002c0
Alex <ihgtsti@gmail.com>
2021-06-02 22:04:50 +0500

    Init

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Currency struct {
+	Valute []InfoOfCurrency
+}
+
+type InfoOfCurrency struct {
+	CharCode string  `xml:"CharCode"`
+	Nominal  uint16  `xml:"Nominal"`
+	Name     string  `xml:"Name"`
+	Value    float64 `xml:"Value"`
+}
+
+type CurrencysMap map[string]Currency
+
+func (val *CurrencysMap) GetOneDayCurrency(date string) error {
+	resp, err := http.Get(QueryAPI + date)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	xmlList, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	xmlList = bytes.ReplaceAll(xmlList, []byte(","), []byte("."))
+	v := Currency{}
+	err = xml.Unmarshal(xmlList[OffsetIgnoreEncoding:], &v)
+	if err != nil {
+		return err
+	}
+	(*val)[date] = v
+	return nil
+}
+
+func (val *CurrencysMap) GetAllDaysCurrency() error {
+
+	t := time.Now()
+	date := t.Format("02/01/2006")
+	for i := 0; i <= int(CountOfDays); i++ {
+		err := val.GetOneDayCurrency(date)
+		if err != nil {
+			return err
+		}
+		t = time.Unix(t.Unix()-CountSecondOfDay, 0)
+		date = t.Format("02/01/2006")
+	}
+	return nil
+}
+
+func (val *CurrencysMap) MinMaxAverage() {
+
+	var count uint32 = 0
+	var infoMax, infoMin InfoOfCurrency
+	var dayMin, dayMax string
+	var min, max, average float64
+	for day, arrVal := range *val {
+		for _, infoVal := range arrVal.Valute {
+			average += infoVal.Value / float64(infoVal.Nominal)
+			count++
+			if min == 0 || min > infoVal.Value/float64(infoVal.Nominal) {
+				min = infoVal.Value / float64(infoVal.Nominal)
+				infoMin = infoVal
+				dayMin = day
+			}
+			if max < infoVal.Value/float64(infoVal.Nominal) {
+				max = infoVal.Value / float64(infoVal.Nominal)
+				infoMax = infoVal
+				dayMax = day
+			}
+		}
+	}
+	fmt.Println(infoMax.Value, infoMax.Name, dayMax)
+	fmt.Println(infoMin.Value, infoMin.Name, dayMin)
+	fmt.Println(average / float64(count))
+}
+
+func main() {
+	var valutes = make(CurrencysMap)
+	err := valutes.GetAllDaysCurrency()
+	if err != nil {
+		log.Fatal(err)
+	}
+	valutes.MinMaxAverage()
+}
---
+++ settings.go
@@ -0,0 +1,6 @@
+package main
+
+const CountSecondOfDay int64 = 86400
+const CountOfDays uint8 = 90
+const OffsetIgnoreEncoding uint8 = 45
+const QueryAPI string = "http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req="
